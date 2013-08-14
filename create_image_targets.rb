249bdeb1ac060fafe3f725e8a4026179807859e9
kift <su.shihjie@gmail.com>
2013-08-14 14:52:45 +0800

    add classes

--- create_image_targets.rb
+++ create_image_targets.rb
@@ -2,3 +2,20 @@ require 'RMagick'
 
 FONTS = ['/Library/Fonts/Jazz LET Fonts', '/Library/Fonts/Microsoft/Lucida Handwriting']
 DOLL_TYPE = "QLRB_Bear"
+
+class ImageTarget
+
+end
+
+
+class ImageTargetWithWhiteFrame
+
+end
+
+class ARImageTarget < ImageTargetWithWhiteFrame
+
+end
+
+class PrintableImageTarget < ImageTargetWithWhiteFrame
+	
+end
